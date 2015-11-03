cdc5a3d6c3206681f000777cec4b3da38867f054
Marc Emmelmann <marc.emmelmann@fokus.fraunhofer.de>
2015-11-03 15:58:39 +0100

    inititial commit -- dockerfile and associated input for dockerizing catalgue broker

---
+++ docker/catalogue-broker/runner.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -x
+
+# Attn.: all paths as they are found on the docker image !!!
+MODELS_FOLDER="/opt/reTHINK/catalogue/model"
+export MODELS_FOLDER
+
+java -jar /opt/reTHINK/catalogue/catalogue_broker/target/rethink-catalogue-broker-0.1-jar-with-dependencies.jar $*
+
+
