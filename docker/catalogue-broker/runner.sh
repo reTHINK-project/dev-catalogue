e0aef3e4fb1c4e808993d888c1adc4d55129671b
Robert Ende <robert.ende@fokus.fraunhofer.de>
2016-02-01 13:51:25 +0100

    updated docker files and readme

--- docker/catalogue-broker/runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -x
-
-# Attn.: all paths as they are found on the docker image !!!
-
-java -jar /opt/reTHINK/catalogue/catalogue_broker/target/rethink-catalogue-broker-2.1-jar-with-dependencies.jar -kp /opt/reTHINK/catalogue/ssl/keystore -tp /opt/reTHINK/catalogue/ssl/keystore $*
-
--- docker/catalogue-database/runner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -x
-
-# Attn.: all paths as they are found on the docker image !!!
-MODELS_FOLDER="/opt/reTHINK/catalogue/model"
-export MODELS_FOLDER
-
-java -jar /opt/reTHINK/catalogue/catalogue_database/target/rethink-catalogue-database-2.1-jar-with-dependencies.jar $*
-
-
