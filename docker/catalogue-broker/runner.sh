da32afaf84ab0d4eb112d22ed936bf7a7f6e4797
Robert Ende <robert.ende@fokus.fraunhofer.de>
2016-02-19 17:17:32 +0100

    Updated version tag & version history

--- docker/catalogue-broker/runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -x
-
-# Attn.: all paths as they are found on the docker image !!!
-
-java -jar /opt/reTHINK/catalogue/catalogue_broker/target/rethink-catalogue-broker-latest-jar-with-dependencies.jar -kp /opt/reTHINK/catalogue/ssl/keystore -tp /opt/reTHINK/catalogue/ssl/keystore $*
-
--- docker/catalogue-database/runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-
-# Attn.: all paths as they are found on the docker image !!!
-
-java -jar /opt/reTHINK/catalogue/catalogue_database/target/rethink-catalogue-database-latest-jar-with-dependencies.jar $*
-
-
