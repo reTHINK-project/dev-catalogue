b8e5f107e9877b797bad53f857850695070bb16d
Robert Ende <robert.ende@fokus.fraunhofer.de>
2016-05-04 09:54:57 +0200

    add Copyright to CatalogueObjectParsingException

--- catalogue_database/src/main/java/eu/rethink/catalogue/database/exception/CatalogueObjectParsingException.java
+++ catalogue_database/src/main/java/eu/rethink/catalogue/database/exception/CatalogueObjectParsingException.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright [2015-2017] Fraunhofer Gesellschaft e.V., Institute for
+ * Open Communication Systems (FOKUS)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package eu.rethink.catalogue.database.exception;
 
 /**
- * Created by Robert Ende on 22.04.16.
+ * A simple Exception to track Catalogue Object parsing errors
  */
 public class CatalogueObjectParsingException extends Exception {
     public CatalogueObjectParsingException(String message) {
